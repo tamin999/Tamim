b175ea35e554e26027802e6b1d4899124875ed2a
tamin999 <jasminalam5298@gmail.com>
2025-10-06 22:53:20 -0600

    Update cdp.js

--- scripts/cmds/cdp.js
+++ scripts/cmds/cdp.js
@@ -1,48 +1,72 @@
 const axios = require("axios");
-const fs = require("fs-extra");
+const fs = require("fs");
+
+const baseApiUrl = async () => {
+  const base = await axios.get(
+    `https://raw.githubusercontent.com/Mostakim0978/D1PT0/refs/heads/main/baseApiUrl.json`
+  );
+  return base.data.api;
+};
+
+const config = {
+  name: "cdp",
+  aliases: ["coupledp"],
+  version: "1.0",
+  author: "Dipto",
+  credits: "Dipto",
+  countDown: 2,
+  role: 0,
+  hasPermission: 0,
+  usePrefix: true,
+  prefix: true,
+  description: "Get random couple DP",
+  category: "image",
+  commandCategory: "image",
+  usages: "coupledp",
+  guide: "{pn}"
+};
+
+const onStart = async ({ event, api }) => {
+  try {
+    const w = await api.sendMessage("Wait Bby <😘", event.threadID);
+
+    const response = await axios.get(`${await baseApiUrl()}/coupledp?random=dipto`);
+    const maleLink = response.data.male;
+    const femaleLink = response.data.female;
+
+    const malePath = `${__dirname}/cache/male.jpg`;
+    const femalePath = `${__dirname}/cache/female.jpg`;
+
+    // Download images as arraybuffer and save using fs
+    const [maleImage, femaleImage] = await Promise.all([
+      axios.get(maleLink, { responseType: "arraybuffer" }),
+      axios.get(femaleLink, { responseType: "arraybuffer" })
+    ]);
+
+    fs.writeFileSync(malePath, maleImage.data);
+    fs.writeFileSync(femalePath, femaleImage.data);
+
+    api.unsendMessage(w.messageID);
+
+    await api.sendMessage({
+      body: "Here's your couple DP <😘",
+      attachment: [
+        fs.createReadStream(malePath),
+        fs.createReadStream(femalePath)
+      ]
+    }, event.threadID, () => {
+      fs.unlinkSync(malePath);
+      fs.unlinkSync(femalePath);
+    }, event.messageID);
+
+  } catch (error) {
+    api.sendMessage(`Error: ${error.message}`, event.threadID, event.messageID);
+    console.error(error);
+  }
+};
 
 module.exports = {
- config: {
- name: "cdp",
- aliases: [],
- version: "1.0",
- author: "XyryllPanget",
- countDown: 5,
- role: 0,
- shortDescription: {
- en: "couple dp"
- },
- longDescription: {
- en: "couple dp"
- },
- category: "love",
- guide: {
- en: "{pn}"
- }
- },
-
- onStart: async function ({ api, event, args }) {
- try {
- const { data } = await axios.get(
- "U+2066https://tanjiro-api.onrender.com/cdp?api_key=tanjiroU+2069"
- );
- const maleImg = await axios.get(data.male, { responseType: "arraybuffer" });
- fs.writeFileSync(__dirname + "/tmp/img1.png", Buffer.from(maleImg.data, "utf-8"));
- const femaleImg = await axios.get(data.female, { responseType: "arraybuffer" });
- fs.writeFileSync(__dirname + "/tmp/img2.png", Buffer.from(femaleImg.data, "utf-8"));
-
- const msg = "「 Here's your pair Dp✨ 」";
- const allImages = [
- fs.createReadStream(__dirname + "/tmp/img1.png"),
- fs.createReadStream(__dirname + "/tmp/img2.png")
- ];
-
- return api.sendMessage({
- body: msg,
- attachment: allImages
- }, event.threadID, event.messageID);
- } catch (error) {
- console.error(error);
- }
- }
+  config,
+  onStart,
+  run: onStart
 };
